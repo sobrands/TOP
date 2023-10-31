80b04f3750db41d9aa10aa9d4e08d16372b6602d
brandon <lim.wx.brandon@gmail.com>
2023-10-31 23:10:02 +0800

    Added in module

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -1,10 +1,12 @@
 import _ from 'lodash';
+import myName from './myName';
 
 function component() {
   const element = document.createElement('div');
 
   // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  // element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.textContent = myName('Cody');
 
   return element;
 }
